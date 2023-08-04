b370265c2698bf059e424a164c71fab49e70ef7c
sugiyat <70905692+sugiyat@users.noreply.github.com>
2023-08-04 09:26:15 -0400

    Finished Inflow PO Data Parse

--- src/main/java/com/intuit/developer/helloworld/controller/InflowController.java
+++ src/main/java/com/intuit/developer/helloworld/controller/InflowController.java
@@ -1,6 +1,11 @@
 package com.intuit.developer.helloworld.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
@@ -17,6 +22,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -49,29 +56,90 @@ public class InflowController {
     @RequestMapping("/getPurchaseOrders")
     public String getPurchaseOrders() {
         try {
-            URL url = new URL(hostURL + companyID + "/purchase-orders");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            JsonArray data = getAllPurchaseOrders(new JsonArray(), "");
 
-            connection.setRequestProperty("Authorization", "Bearer " + token);
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Accept", "application/json;version=2021-04-26");
-            connection.setRequestMethod("GET");
+            String test = "";
+            for (JsonElement item : data) {
+                JsonObject purchaseOrder = item.getAsJsonObject();
+                test += "> Purchase Order Number: " + purchaseOrder.get("orderNumber") + "\n";
 
-            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String output;
+                JsonElement vendor = purchaseOrder.get("vendor");
+                if (vendor != null) {
+                    test += "--- Vendor: " + vendor.getAsJsonObject().get("name") + "\n";
+                }
 
-            StringBuffer response = new StringBuffer();
-            while ((output = input.readLine()) != null)
-                response.append(output);
-            input.close();
+                test += "--- Order Date: " + purchaseOrder.get("orderDate") + "\n";
+                test += "--- Request Ship Date: " + purchaseOrder.get("requestShipDate") + "\n";
 
-            return response.toString();
+                JsonElement customFields = purchaseOrder.get("customFields");
+                if (customFields != null) {
+                    test += "--- Control #: " + customFields.toString() + "\n";
+//                    test += "--- Confirmed? (Y/N-Date): " + purchaseOrder.get("customFields").toString() + "\n";
+                }
+
+
+            }
+
+            return test;
         } catch (MalformedURLException e) {
             logger.error("Error while creating URL :: " + e.getMessage());
             return new JSONObject().put("response", failureMsg).toString();
         } catch (IOException e) {
             logger.error("Error while connecting :: " + e.getMessage());
             return new JSONObject().put("response", failureMsg).toString();
+        } catch (InterruptedException e) {
+            logger.error("Error while sleeping :: " + e.getMessage());
+            return new JSONObject().put("response", failureMsg).toString();
         }
+    }
+
+    private JsonArray getAllPurchaseOrders(JsonArray data, String afterId) throws MalformedURLException, IOException, InterruptedException {
+        LocalDate today = LocalDate.now();
+        String fromDate = today.minusMonths(18).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String toDate = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        String urlString = hostURL + companyID + "/purchase-orders" + "?count=100"
+                + "&filter[orderDate]={\"fromDate\":\""
+                + fromDate + "\",\"toDate\":\""
+                + toDate + "\"}";
+
+        if (!afterId.isEmpty())
+            urlString += "&after=" + afterId;
+
+        URL url = new URL(urlString);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json;version=2021-04-26");
+        connection.setRequestMethod("GET");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String output;
+
+        StringBuffer response = new StringBuffer();
+        while ((output = input.readLine()) != null)
+            response.append(output);
+        input.close();
+
+        JsonParser jsonParser = new JsonParser();
+        JsonArray page = (JsonArray) jsonParser.parse(response.toString());
+
+        int size = page.size();
+        if (size <= 0 && !afterId.isEmpty())
+            return data;
+
+        for (JsonElement element : page)
+            data.add(element);
+
+        String lastPOId = page.get(size - 1).getAsJsonObject().get("purchaseOrderId").getAsString();
+
+        logger.debug("UPDATE: NEXT PAGE AfterId :: " + afterId + "\n" + urlString);
+
+        Thread.sleep(2000);
+        return getAllPurchaseOrders(data, lastPOId);
+
+//        return page;
     }
 }
